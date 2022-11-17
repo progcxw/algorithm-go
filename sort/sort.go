57934a482bb326046659e4e4aac8f4ba583edd2b
progcxw <chengxuwu@nova.net.cn>
2022-11-17 20:35:55 +0800

    heap sort

--- sort/sort.go
+++ sort/sort.go
@@ -23,3 +23,50 @@ func QuickSort(nums []int) {
 	QuickSort(nums[:pos])
 	QuickSort(nums[pos+1:])
 }
+
+// HeapSort 堆排
+func HeapSort(nums []int) {
+	// 从最后一个非叶子节点开始建立最大堆
+	for i := len(nums)/2 - 1; i > 0; i-- {
+		sink(nums, i)
+	}
+
+	// 最大堆形成，根节点为最大值，直接将其挪到最后
+	for i := len(nums) - 1; i > 0; i-- {
+		nums[0], nums[i] = nums[i], nums[0]
+		// 除了最后的节点外，再建一次最大堆，此时只用从首项开始sink
+		sink(nums[:i], 0)
+	}
+}
+
+func sink(nums []int, i int) {
+	for {
+		// 使用biggest记录当前子树中最大节点的index
+		biggest := i
+		length := len(nums)
+		// 注意需要判断子节点index是否越界
+		lChild := 2*i + 1
+		rChild := 2*i + 2
+
+		// 先找出i节点与左子节点中最大值的index
+		if lChild < length && nums[i] < nums[lChild] {
+			biggest = lChild
+		}
+
+		// 此时的biggest储存了i节点与左子节点中的最大值index， 再将该index节点与右子节点比较，得到三者中最大节点
+		if rChild < length && nums[biggest] < nums[rChild] {
+			biggest = rChild
+		}
+
+		if biggest == i {
+			// 当前子树是最大堆，直接break
+			break
+		}
+
+		// swap 最大节点与i节点
+		nums[i], nums[biggest] = nums[biggest], nums[i]
+
+		// 此时被换下来的节点的子节点可能比它大，往下sink
+		i = biggest
+	}
+}
