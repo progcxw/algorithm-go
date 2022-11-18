8b11e1ed50308eba809935cf3ddb56d44f15c69b
progcxw <chengxuwu@nova.net.cn>
2022-11-18 18:01:25 +0800

    bubble sort and selection sort

--- sort/sort.go
+++ sort/sort.go
@@ -1,5 +1,42 @@
 package sort
 
+// BubbleSort 冒泡排序
+func BubbleSort(nums []int) {
+	for j := len(nums) - 1; j > 0; j-- {
+		// 每次i循环转完，j位置上的数位置定好，所以j从数组尾项遍历到首项时排序完毕
+		for i := 0; i < j; i++ {
+			// 比j大的位置都已排好序，只用遍历0到j
+			if nums[i] > nums[i+1] {
+				nums[i], nums[i+1] = nums[i+1], nums[i]
+			}
+		}
+	}
+}
+
+// SelectionSort 选择排序
+func SelectionSort(nums []int) {
+	for j := 0; j < len(nums)-1; j++ {
+		// 找到j到尾项中最小的数字，将其与nums[j]交换位置
+		smallest := j
+		for i := j; i < len(nums); i++ {
+			if nums[i] < nums[smallest] {
+				smallest = i
+			}
+		}
+		nums[j], nums[smallest] = nums[smallest], nums[j]
+	}
+}
+
+// InsertionSort 插入排序
+func InsertionSort(nums []int) {
+	for j := 1; j < len(nums); j++ {
+		// 把首项视为一个有序数组，从第二项开始将值插入到该有序数组中
+		for i := 0; i <= j; i++ {
+			if
+		}
+	}
+}
+
 // QuickSort 快速排序
 func QuickSort(nums []int) {
 	length := len(nums)
@@ -59,7 +96,7 @@ func sink(nums []int, i int) {
 		}
 
 		if biggest == i {
-			// 当前子树是最大堆，直接break
+			// 当前子树是最大堆，无需swap，直接break
 			break
 		}
 
